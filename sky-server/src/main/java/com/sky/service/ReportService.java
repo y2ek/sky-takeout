2daaa668bd14b2cc0b2f1a69c6ce32cc7ae31374
Blink <2277980456@qq.com>
2025-06-22 20:38:39 +0800

    【特性新增】营业额数据统计功能代码开发

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,20 @@
+package com.sky.service;
+
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
+import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+public interface ReportService {
+
+    /**
+     * 统计指定时间区间内的营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
